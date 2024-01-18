27a32433344339bed0eed9136b56124463c3e5c0
profeguerra <cursosluisguerra@gmail.com>
2024-01-17 20:50:46 -0600

    Tipos de datos, numeros, operaciones aritmeticas y funciones matematicas, en python

---
+++ 002.numbers.py
@@ -0,0 +1,12 @@
+'''
+002. Numbers
+En python tenemos 2 tipos de numeros, enteros y flotantes, aunque tambien se usa uno llamado "complex"
+'''
+
+print(type(2024))
+print(type(12.12))
+print(type(5j))
+
+print(2024)
+print(12.12)
+print(5j)
