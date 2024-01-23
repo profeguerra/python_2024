85753d6a2f389ae3f13fc4a916123a08bc46da84
profeguerra <cursosluisguerra@gmail.com>
2024-01-22 19:14:56 -0600

    Data type execises

---
+++ 003.string_methods.py
@@ -0,0 +1,17 @@
+'''
+003 String methods
+son metodos que ya existen y podemos usar para las cadenas de texto
+
+function(params)
+object.methods()
+'''
+
+name = 'luis ricardo guerra arroyo'
+
+
+print("name")
+print(name.capitalize())
+print(name.count('i'))
+print(name.replace(' ', ''))
+print(name.title())
+
