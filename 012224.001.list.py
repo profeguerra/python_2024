df7e0fd523f711027001bd84b76b5a77b3d14519
profeguerra <cursosluisguerra@gmail.com>
2024-01-22 20:48:59 -0600

    Aprendiendo listas y sus metodos

---
+++ 012224.001.list.py
@@ -0,0 +1,44 @@
+'''
+001 List
+- 
+'''
+
+list_my_students = [
+    'Maggie',     #0 1
+    'Ibrahim',    #1 2
+    'Paulina',    #2 3
+    'Octavio',    #3 4
+    'Ulises',     #4 5
+    2024,         #5 6  
+    2.50,         #6 7
+    'Paulina'     #7 8
+]
+
+'''
+print(list_my_students)
+print(list_my_students[0])
+print(list_my_students[1])
+print(list_my_students[2])
+print(list_my_students[3])
+print(list_my_students[4])
+list_my_students.append("Luis")
+list_my_students.insert(0, 'Isela')
+list_my_students.extend(['Javier', 'Isela', 'Limon'])
+'''
+
+print(list_my_students)
+#remove elements
+#list_my_students.pop()
+#list_my_students.pop(1)
+list_my_students.remove("Paulina")
+#list_my_students.clear()
+#print(len(list_my_students))
+
+print(list_my_students)
+
+'''
+for student in list_my_students:
+    print(student)
+'''
+
+
