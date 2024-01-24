5f40097d8cdd69012cf673a8d8a183c06dd97295
profeguerra <cursosluisguerra@gmail.com>
2024-01-23 19:25:31 -0600

    Adding list description, and example

--- 012224.001.list.py
+++ 012224.001.list.py
@@ -1,6 +1,11 @@
 '''
 001 List
-- 
+- Se usan para guardar multiples datos en una variable
+- Los datos que puede guardar la lista son, int, float, string, boolean, listas
+- las listas usan los [] (square brakes)
+- las listas pueden ser ordenadas, cambiadas, y permiten duplicados 
+- las listas son indexadas e inician con el [0] 
+- las listas pueden ser arreglos en otros lenguajes de programacion
 '''
 
 list_my_students = [
@@ -9,8 +14,8 @@ list_my_students = [
     'Paulina',    #2 3
     'Octavio',    #3 4
     'Ulises',     #4 5
-    2024,         #5 6  
-    2.50,          #6 7
+    #2024,         #5 6  
+    #2.50,          #6 7
     'Paulina'     #7 8
 ]
 
@@ -30,18 +35,22 @@ list_my_students.extend(['Javier', 'Isela', 'Limon'])
 #remove elements
 #list_my_students.pop()
 #list_my_students.pop(1)
-list_my_students.remove("Paulina")
+#list_my_students.remove("Paulina")
 #list_my_students.clear()
 #print(len(list_my_students))
 
 print(list_my_students)
-
+list_my_students.sort()
+print(list_my_students)
 
 '''
 for student in list_my_students:
     print(student)
 '''
 
-# test comment 
+
+#ordenando una lista 
+
+
 
 
