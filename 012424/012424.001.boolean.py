dd6e7a0f7a15d19ee4f075fbac8e5a263bf15699
profeguerra <cursosluisguerra@gmail.com>
2024-01-24 20:43:04 -0600

    Clase Enero 24, boleanos, condicionales, operadores ternarios, operadores logicos y is vs ==

---
+++ 012424/012424.001.boolean.py
@@ -0,0 +1,12 @@
+'''
+001 Boolean - George Boolean 
+- Tiene dos valores, True / False
+- debe de ir con mayuscula la primer letra
+'''
+
+number = 10 
+
+print(number > 5)
+print(number < 5)
+
+print(number == 10)
