b022bbaede45fb369498df82c1812666c754d3f9
profeguerra <cursosluisguerra@gmail.com>
2024-01-26 20:12:48 -0600

    se dejan dos listas, una donde estan los repetidos y una mas de no repetidos #1

---
+++ 012624.001.duplicates.py
@@ -0,0 +1,38 @@
+'''
+001. duplicates
+'''
+
+my_list = ['a','b','c','d','d','e','f','f','g','g','g','g']
+
+#print(my_list)
+
+'''
+repetidos = []
+no_repetidos = []
+
+for item in my_list:
+    #print(item)
+    if my_list.count(item) > 1:
+        #print(item)
+        repetidos.append(item)
+    else:
+        #print(item)
+        no_repetidos.append(item)
+
+
+print(repetidos)
+print(no_repetidos)
+
+
+repetidos = []
+
+for item in my_list:
+    if my_list.count(item) > 1:
+        repetidos.append(item)
+        my_list.remove(item)#
+
+print(repetidos)
+print(my_list)
+
+
+'''
