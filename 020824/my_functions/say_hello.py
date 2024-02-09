6ed8b6eb4ca308f45ac2877e8ac56bf38002f3f3
profeguerra <cursosluisguerra@gmail.com>
2024-02-09 12:08:39 -0600

    Descripcion de las funciones *.md

---
+++ 020824/my_functions/say_hello.py
@@ -0,0 +1,12 @@
+def say_hello():
+    '''
+    Info: esta funcion recibe una pregunta que se mostrara en la termina y el valor que ingresa el usuario, se guardara e una variable 
+    \n
+    input data: 
+        - str: question
+        - str: variable_name
+    \n
+    output data: 
+        - str: variable_name
+    '''
+    return "Hello"
